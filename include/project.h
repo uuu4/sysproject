3dec7deb41145407c1944458340b9486283a01a5
Ali Emre Aydın <alinintopsecreti@gmail.com>
2025-04-06 02:07:53 +0300

    model.c

---
+++ include/project.h
@@ -0,0 +1,50 @@
+#ifndef PROJECT_H
+#define PROJECT_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <semaphore.h>
+#include <sys/types.h>
+#include <stddef.h>
+#include <unistd.h>
+
+/* Sabit tanımlamalar */
+#define BUF_SIZE 4096
+#define SHARED_FILE_PATH "mymsgbuf"
+
+/* Hata durumunda çıkış makrosu */
+#define errExit(msg)        \
+    do {                    \
+        perror(msg);        \
+        exit(EXIT_FAILURE); \
+    } while (0)
+
+/* Paylaşılan bellek yapısı */
+typedef struct shmbuf {
+    sem_t sem;     /* Erişim kontrolü için semafor */
+    size_t cnt;    /* msgbuf içinde kullanılan bayt sayısı */
+    int fd;        /* Dosya tanımlayıcısı */
+    char msgbuf[]; /* Aktarılacak mesaj verisi */
+} ShmBuf;
+
+/* Shell süreç bilgilerini tutan yapı */
+typedef struct {
+    pid_t pid;          /* Süreç ID'si */
+    char command[256];  /* Çalıştırılan komut */
+    int status;         /* Süreç durumu (çalışıyor/sonlandı) */
+} ProcessInfo;
+
+/* Model fonksiyon prototipleri */
+ShmBuf* buf_init(void);
+int execute_command(const char *command);
+int model_send_message(ShmBuf *shmp, const char *msg);
+int model_read_messages(ShmBuf *shmp, char *buffer, size_t bufsize);
+
+/* Controller fonksiyon prototipleri */
+void controller_parse_input(const char *input);
+
+/* View (GUI) fonksiyon prototipleri */
+void view_update_terminal(const char *output);
+void view_display_message(const char *msg);
+
+#endif /* PROJECT_H */
--- src/model.c
+++ src/model.c
@@ -6,23 +6,12 @@
 #include <sys/mman.h>
 #include <sys/wait.h>
 #include <unistd.h>
+#include "../include/project.h"
 
 #define BUF_SIZE 4096
+#define MAX_ARGS 64
 #define SHARED_FILE_PATH "mymsgbuf"
 
-#define errExit(msg)        \
-    do {                    \
-        perror(msg);        \
-        exit(EXIT_FAILURE); \
-    } while (0)
-
-/* YOU CAN CHANGE THIS */
-typedef struct shmbuf {
-    sem_t sem;     /** controls read/write */
-    size_t cnt;    /** number of bytes used in 'msgbuf' */
-    int fd;        /** file descriptor*/
-    char msgbuf[]; /* data being transferred */
-} ShmBuf;
 
 struct shmbuf *buf_init() {
     struct shmbuf *shmp;
@@ -46,6 +35,76 @@ struct shmbuf *buf_init() {
     }
 
     return shmp;
+}
+/**
+ * execute_command - Girilen komut satırını çalıştırır ve çıktısını yakalar.
+ * @command: Çalıştırılacak komut (örn: "ls -l /home")
+ *
+ * Return: Çocuk sürecin çıkış durumunu döndürür.
+ */
+int execute_command(const char *command) {
+    int pipefd[2];
+    char buffer[BUF_SIZE];
+    pid_t pid;
+
+    // Pipe oluşturma
+    if (pipe(pipefd) == -1) {
+        perror("pipe error");
+        return -1;
+    }
+
+    pid = fork();
+    if (pid < 0) {
+        perror("fork error");
+        return -1;
+    }
+
+    if (pid == 0) {
+        // Çocuk süreç: stdout ve stderr'i pipe'a yönlendir
+        close(pipefd[0]); // Okuma ucunu kapat
+        dup2(pipefd[1], STDOUT_FILENO);
+        dup2(pipefd[1], STDERR_FILENO);
+        close(pipefd[1]);
+
+        // Komutu tokenize etme
+        char *args[MAX_ARGS];
+        char *cmd_copy = strdup(command);
+        if (!cmd_copy) {
+            perror("strdup error");
+            exit(EXIT_FAILURE);
+        }
+        int i = 0;
+        char *token = strtok(cmd_copy, " ");
+        while (token != NULL && i < MAX_ARGS - 1) {
+            args[i++] = token;
+            token = strtok(NULL, " ");
+        }
+        args[i] = NULL; // Argüman listesini sonlandır
+
+        // Komutu çalıştır (execvp komut dizisinin ilk elemanına göre çalışır)
+        execvp(args[0], args);
+        // Eğer execvp başarısız olursa hata mesajı yazdır ve çık
+        perror("execvp failed");
+        free(cmd_copy);
+        exit(EXIT_FAILURE);
+    } else {
+        // Ebeveyn süreç: yazma ucunu kapatıp, çocuk sürecin çıktısını oku
+        close(pipefd[1]);
+
+        int nbytes = read(pipefd[0], buffer, sizeof(buffer) - 1);
+        if (nbytes < 0) {
+            perror("read error");
+        } else {
+            buffer[nbytes] = '\0';
+            printf("Komut çıktısı:\n%s\n", buffer);
+        }
+        close(pipefd[0]);
+
+        // Çocuk süreci bekle ve çıkış durumunu döndür
+        int status;
+        waitpid(pid, &status, 0);
+        return status;
+    }
 }
 
 int main() {
@@ -73,3 +132,4 @@ int main() {
     shm_unlink(SHARED_FILE_PATH);
     return 0;
 }
+
